a3cad840cd8237595b6d040f4d89aa6cec2223f1
cerico <garethrobertlee@gmail.com>
2017-08-10 14:33:33 +0100

    rm cljs target

--- target/shadow-cljs/npm/dev/cljs-runtime/demo.foo.js
+++
@@ -1,6 +0,0 @@
-goog.provide('demo.foo');
-goog.require('cljs.core');
-goog.require('cljs.core');
-demo.foo.hello = (function demo$foo$hello(who){
-return [cljs.core.str.cljs$core$IFn$_invoke$arity$1("Hello, "),cljs.core.str.cljs$core$IFn$_invoke$arity$1(who),cljs.core.str.cljs$core$IFn$_invoke$arity$1("!")].join('');
-});
